2caba8fefa2508c98422f0c8f8341fdfa125b3f3
. <hardik2@deloitte.com>
2024-09-18 02:40:32 +0530

    second commit

--- index.js
+++ index.js
@@ -259,9 +259,9 @@ app.post("/addTodo", (req, res) => {
 app.get("/todo/:id", (req, res) => {
     const index = req.params.id;
     const data = obj.getTodoById(index);
-    if(data == -1){
+    if (data == -1) {
         res.status(411).json({
-            msg:"enter correct index"
+            msg: "enter correct index"
         })
     }
     res.json({
@@ -272,9 +272,9 @@ app.get("/todo/:id", (req, res) => {
 app.delete("/todo/:id", (req, res) => {
     const index = req.params.id;
     const data = obj.deleteTodoItem(index);
-    if(data == -1){
+    if (data == -1) {
         res.status(411).json({
-            msg:"enter correct index"
+            msg: "enter correct index"
         })
     }
     res.json({
@@ -286,12 +286,118 @@ app.put("/todo/:id", (req, res) => {
     const index = req.params.id;
     const updatedData = req.body;
     const data = obj.updateExistingTodo(index, updatedData)
-    if(data == -1){
+    if (data == -1) {
         res.status(411).json({
-            msg:"enter correct index"
+            msg: "enter correct index"
         })
     }
     res.json({
         updatedData: data
+    })
+})
+
+/**
+  You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
+  - Don't need to use any database to store the data.
+
+  - Save the users and their signup/login data in an array in a variable
+  - You can store the passwords in plain text (as is) in the variable for now
+
+  The expected API endpoints are defined below,
+  1. POST /signup - User Signup
+    Description: Allows users to create an account. This should be stored in an array on the server, and a unique id should be generated for every new user that is added.
+    Request Body: JSON object with username, password, firstName and lastName fields.
+    Response: 201 Created if successful, or 400 Bad Request if the username already exists.
+    Example: POST http://localhost:3000/signup
+
+  2. POST /login - User Login
+    Description: Gets user back their details like firstname, lastname and id
+    Request Body: JSON object with username and password fields.
+    Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
+    Example: POST http://localhost:3000/login
+
+  3. GET /data - Fetch all user's names and ids from the server (Protected route)
+    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
+    The users username and password should be fetched from the headers and checked before the array is returned
+    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
+    Example: GET http://localhost:3000/data
+
+  - For any other route not defined in the server return 404
+
+  Testing the server - run `npm run test-authenticationServer` command in terminal
+ */
+
+
+let userDataArray = [];
+let id = 0;
+function toCheckUserExisted(username) {
+    for (let i = 0; i < userDataArray.length; i++) {
+        if (userDataArray[i].username == username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+app.post("/signup", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    const isUsernameExist = toCheckUserExisted(req.body.username);
+    if (isUsernameExist) {
+        res.status(400).json({
+            msg: "user already existed"
+        })
+    } else {
+        data.id = ++id;
+        userDataArray.push(data);
+        res.json({
+            msg: "User added",
+            user: data
+        })
+    }
+})
+function toCheckAuthentication(username, password) {
+    for (let i = 0; i < userDataArray.length; i++) {
+        if (userDataArray[i].username == username) {
+            if (userDataArray[i].password == password) {
+                return userDataArray[i];
+            }
+        }
+    }
+    return false;
+}
+
+app.post("/login", (req, res) => {
+    const data = req.body;
+    const isValidUser = toCheckAuthentication(req.body.username, req.body.password);
+    if (isValidUser) {
+        res.json({
+            msg: "valid user",
+            user: isValidUser
+        })
+    }
+    res.json({
+        msg: "Invalid user"
+    })
+})
+
+app.get("/fetch", (req, res) => {
+    const { username, password } = req.headers;
+    const isValidUser = toCheckAuthentication(username, password);
+    if (isValidUser) {
+        let userNeedsToReturn = [];
+        for (let i = 0; i < userDataArray.length; i++) {
+            userNeedsToReturn.push({
+                firstName: userDataArray[i].firstname,
+                lastName: userDataArray[i].lastname,
+                username: userDataArray[i].username
+            })
+        }
+        res.json({
+            userNeedsToReturn
+        })
+    }
+    res.status(401).json({
+        msg: "invalid credentials"
     })
 })
